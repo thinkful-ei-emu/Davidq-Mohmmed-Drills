5e352fd141c6eee5ad94b5e03a2ad2087a0670d3
mak20th <mak20th@gmail.com>
2019-06-26 11:25:37 -0500

    Drill 2 completed

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Bomb from './state-drills/Bomb';
 
 function App() {
   return (
-    <main className='App'>
-      {/* content goes here */}
+    <main className='App'>      
+      <Bomb/>
     </main>
   );
 }
